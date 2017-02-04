3a2d35d7a6368994887af150ef3a648f7d493f41
Jay Liang <jay.liang@ugeez.cn>
2017-02-04 13:52:04 +0800

    add MailUtil test case

---
+++ test/utils/MailUtil.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Jay on 2017/1/13.
+ */
+
+var assert = require("assert");
+var Redis = require("weroll/model/Redis");
+var MailUtil = require("weroll/utils/MailUtil");
+var TemplateLib = require("weroll/utils/TemplateLib");
+
+describe('MailUtil',function() {
+
+    before(function (done) {
+        //do something you need before run test cases.
+        Redis.findKeysAndDel("*", function(err) {
+            done(err);
+        });
+    });
+
+    //each it code block is a test case
+    it('init', async function(){
+        var config = {
+            smtp:{
+                user:"developer@magicfish.cn",
+                password:"xxxxxxxxx",
+                host:"smtp.xxxx.com",
+                port:465,
+                ssl:true
+            },
+            sender:"developer@magicfish.cn",
+            senderName:"Robot",
+            simulate:true
+        };
+        MailUtil.init(config);
+        assert.ok(true);
+    });
+
+    it('send', async function(){
+        try {
+            await MailUtil.send("xxxxxx@qq.com", "Hello", "Hi user, xxxxx...");
+            assert.ok(true);
+        } catch (exp) {
+            assert.equal(exp, undefined);
+        }
+    });
+});
--- test/utils/PhoneValidationCode.js
+++ test/utils/PhoneValidationCode.js
@@ -14,8 +14,6 @@ describe('PhoneValidationCode',function() {
         //do something you need before run test cases.
 
         var config = {
-            api:"http://utf8.sms.webchinese.cn/?Uid=淘迪叔叔",
-            secret:"87854cc77db024eddf72",
             limit: {
                 duration:1000,   //milli sec
                 maxPerDay:2
